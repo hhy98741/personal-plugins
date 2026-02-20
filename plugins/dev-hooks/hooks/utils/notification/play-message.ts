ac452181c61bb79060d4db2e4a95db0487906a70
hhy98741 <gitcommitter@eskietech.com>
2026-02-19 21:51:27 -0800

    use spawn instead of exec

--- plugins/dev-hooks/hooks/utils/notification/play-message.ts
+++ plugins/dev-hooks/hooks/utils/notification/play-message.ts
@@ -1,10 +1,9 @@
-import { exec } from 'child_process';
 import { join } from "path";
 
 export function play(messageFile: string): void {
   try {
     const pathToFile = join(import.meta.dir, "audio-files", messageFile);
-    exec(`afplay ${pathToFile}`);
+    Bun.spawn(["afplay", pathToFile]);
   } catch {
     // Fail silently
   }
